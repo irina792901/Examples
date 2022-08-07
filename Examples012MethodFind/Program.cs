e2165fc70aceb300d894d36bcc0816b48e21af35
Irina_Shipunova <irina_shipunova@list.ru>
2022-08-07 18:33:16 +0700

    Добавила пример нахождения элемента массива

---
+++ Examples012MethodFind/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 11, 13, 51, 68, 97, 4, 45, 912 };
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
