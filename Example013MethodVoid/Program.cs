3ffdfa09ab836d88dc9dc410c9345f90d827386b
Irina_Shipunova <irina_shipunova@list.ru>
2022-08-07 20:10:06 +0700

    Добавила поиск индекса заданного элемента при помощи метода

--- Example013MethodVoid/Program.cs
+++ Example013MethodVoid/Program.cs
@@ -21,3 +21,22 @@ void PrintArray(int[] col)
 int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // последнее вхождение
+        }
+        index++;
+    }
+return position;
+}
+Console.WriteLine(); //пустая строка отделить массив
+int pos = IndexOf(array, 2);
+Console.WriteLine(pos);
